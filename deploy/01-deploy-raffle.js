fcf6bf14e74aa58961b407504e704f5ba25e9a96
Asif Mahmoud <asifmahmoud787@gmail.com>
2022-09-08 17:11:42 +0100

    test error resolved

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -11,13 +11,13 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     let vrfCoordinatorV2Address, subscriptionId;
 
     if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await ethers.getContract("vrfCoordinatorV2Mock");
-        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
-        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+        const transactionResponse = await VRFCoordinatorV2Mock.createSubscription();
         const transactionReceipt = await transactionResponse.wait(1);
         subscriptionId = transactionReceipt.events[0].args.subId;
         // Fund the subcription
-        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_FUND_AMOUNT);
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_FUND_AMOUNT);
     } else {
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
         subscriptionId = networkConfig[chainId]["subscriptionId"];
